2d3bf844e638ed97ad8e6724ca61b233c1e0c5a2
Vinicios Wentz <vinicios@wentz.io>
2021-09-14 22:12:10 -0300

    feat: user registration

---
+++ lib/rumbl/accounts.ex
@@ -0,0 +1,30 @@
+defmodule Rumbl.Accounts do
+  alias Rumbl.Repo
+  alias Rumbl.Accounts.User
+
+  def change_user(%User{} = user) do
+    User.changeset(user, %{})
+  end
+
+  def create_user(attrs \\ %{}) do
+    %User{}
+    |> User.changeset(attrs)
+    |> Repo.insert()
+  end
+
+  def list_users do
+    Repo.all(User)
+  end
+
+  def get_user(id) do
+    Repo.get(User, id)
+  end
+
+  def get_user!(id) do
+    Repo.get!(User, id)
+  end
+
+  def get_user_by(params) do
+    Repo.get_by(User, params)
+  end
+end
