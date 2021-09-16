ca928a74c1e643c14f9daea573cf20df1fa6054c
Vinicios Wentz <vinicios@wentz.io>
2021-09-15 23:05:54 -0300

    feat: logout

--- lib/rumbl/accounts.ex
+++ lib/rumbl/accounts.ex
@@ -37,4 +37,20 @@ defmodule Rumbl.Accounts do
   def get_user_by(params) do
     Repo.get_by(User, params)
   end
+
+  def authenticate_by_username_and_password(username, given_pass) do
+    user = get_user_by(username: username)
+
+    cond do
+      user && Pbkdf2.verify_pass(given_pass, user.password_hash) ->
+        {:ok, user}
+
+      user ->
+        {:error, :unauthorized}
+
+      true ->
+        Pbkdf2.no_user_verify()
+        {:error, :not_found}
+    end
+  end
 end
--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -7,6 +7,8 @@ defmodule RumblWeb.Router do
     plug(:fetch_flash)
     plug(:protect_from_forgery)
     plug(:put_secure_browser_headers)
+
+    plug(RumblWeb.Auth)
   end
 
   pipeline :api do
@@ -19,6 +21,7 @@ defmodule RumblWeb.Router do
     get("/", PageController, :index)
 
     resources("/users", UserController, only: [:index, :show, :new, :create])
+    resources("/session", SessionController, only: [:new, :create, :delete])
   end
 
   # Other scopes may use custom stacks.
