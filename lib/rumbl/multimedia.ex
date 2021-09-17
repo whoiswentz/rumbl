35b99ecf838ca012328d8da75fa1d09f58adc97b
Vinicios Wentz <vinicios@wentz.io>
2021-09-17 10:23:06 -0300

    feat: working with relationship

---
+++ lib/rumbl/multimedia.ex
@@ -0,0 +1,48 @@
+defmodule Rumbl.Multimedia do
+  import Ecto.Query, warn: false
+  alias Rumbl.Repo
+
+  alias Rumbl.Multimedia.Video
+  alias Rumbl.Accounts
+
+  def list_videos do
+    Repo.all(Video)
+  end
+
+  def get_video!(id), do: Repo.get!(Video, id)
+
+  def create_video(%Accounts.User{} = user, attrs \\ %{}) do
+    %Video{}
+    |> Video.changeset(attrs)
+    |> Ecto.Changeset.put_assoc(:user, user)
+    |> Repo.insert()
+  end
+
+  def update_video(%Video{} = video, attrs) do
+    video
+    |> Video.changeset(attrs)
+    |> Repo.update()
+  end
+
+  def delete_video(%Video{} = video) do
+    Repo.delete(video)
+  end
+
+  def change_video(%Video{} = video, attrs \\ %{}) do
+    Video.changeset(video, attrs)
+  end
+
+  def list_user_videos(%Accounts.User{} = user) do
+    Video
+    |> user_videos_query(user)
+    |> Repo.all()
+  end
+
+  def get_user_video!(%Accounts.User{} = user, id) do
+    Video |> user_videos_query(user) |> Repo.get!(id)
+  end
+
+  defp user_videos_query(query, %Accounts.User{id: user_id}) do
+    from(v in query, where: v.user_id == ^user_id)
+  end
+end
--- lib/rumbl_web/router.ex
+++ lib/rumbl_web/router.ex
@@ -21,7 +21,13 @@ defmodule RumblWeb.Router do
     get("/", PageController, :index)
 
     resources("/users", UserController, only: [:index, :show, :new, :create])
-    resources("/session", SessionController, only: [:new, :create, :delete])
+    resources("/sessions", SessionController, only: [:new, :create, :delete])
+  end
+
+  scope "/manage", RumblWeb do
+    pipe_through([:browser, :authenticate_user])
+
+    resources("/videos", VideoController)
   end
 
   # Other scopes may use custom stacks.
